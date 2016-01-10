e1ebfb475168533beeebaf067d9c4a863bc7b99e
Tim <timevko@gmail.com>
2016-01-10 15:47:07 -0500

    initial commit

---
+++ serviceworker.js
@@ -0,0 +1,30 @@
+// The files we want to cache
+var CACHE_NAME = 'site-cache-v1';
+var urlsToCache = [
+  '/'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+    // Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
+
+			return fetch(event.request);
+		})
+	);
+});

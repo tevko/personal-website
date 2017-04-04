f59298ca0d3e00bc3bfa8cb755bdfaa564319b00
Tim <timevko@gmail.com>
2017-04-03 22:13:03 -0400

    fix sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,46 +1,45 @@
 // The files we want to cache
-var CACHE_NAME = 'site-cache-v3';
-var urlsToCache = [
+const CACHE_NAME = 'site-cache-v3';
+
+const urlsToCache = [
   '/',
   '/blog',
-  '/how-this-blog-was-made/'
-];
+  '/blog/how-this-blog-was-made/',
+  '/blog/wp-content/themes/blog/public/css/main.min.css',
+  'https://fonts.googleapis.com/css?family=Open+Sans:400,700'
+ ];
 
-// Set the callback for the install step
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
     // Perform install steps
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
-	);
+    event.waitUntil(caches.open(CACHE_NAME).then(cache =>
+        Promise.all(
+             urlsToCache.map((url) => {
+                const request = new Request(url);
+                return fetch(request).then(response => cache.put(request, response));
+            })
+        )
+    ))
 });
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-		.then(function(response) {
-			// Cache hit - return response
-			if (response) {
-				return response;
-			}
-
-			return fetch(event.request);
-		})
-	);
+// respond with matches from cache
+self.addEventListener('fetch', (event) => {
+    if (urlsToCache.some(url => event.request.url.indexOf(url) !== -1)) {
+        event.respondWith(caches.match(event.request).then(response => {
+            if (!response) {
+                const request = new Request(event.request.url);
+                return fetch(request)
+            }
+            return response;
+        }));
+    }
 });
 
-this.addEventListener('activate', function(event) {
-	var cacheWhitelist = [CACHE_NAME];
+// remove old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
 
-	event.waitUntil(
-		caches.keys().then(function(keyList) {
-			return Promise.all(keyList.map(function(key) {
-				if (cacheWhitelist.indexOf(key) === -1) {
-					return caches.delete(key);
-				}
-			}));
-		})
-	);
+    event.waitUntil(caches.keys()
+    .then(keyList =>
+        Promise.all(keyList.map(key =>
+            cacheWhitelist.indexOf(key) === -1 ? caches.delete(key) : false))));
 });
